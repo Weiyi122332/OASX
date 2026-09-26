4ada9f2454f34b609adc7f2ede839bc0ce2c5d29
Weiyi122332 <90968534+Weiyi122332@users.noreply.github.com>
2026-09-26 21:55:26 +0800

    feat: label demon retreat start settings

--- lib/translation/cn_parts/cn_demon_retreat_config.dart
+++ lib/translation/cn_parts/cn_demon_retreat_config.dart
@@ -3,5 +3,10 @@ part of i18n;
 
 final Map<String, String> _cn_demon_retreat_config = {
   'demon_retreat_time': '首领退治时间',
-  'custom_run_time': '自定义运行时间'
+  'custom_run_time': '自定义运行时间',
+  'demon_retreat_start': '首领退治开启',
+  'auto_start': '自动开启首领退治',
+  'difficulty': '开启难度（1–8）',
+  'demon_retreat_auto_start_help': '进入首领退治后，若出现难度选择和开启按钮，自动选择难度并开启。',
+  'demon_retreat_difficulty_help': '选择 1–8 档难度；若当前无法选择该难度，则停止开启。'
 };
